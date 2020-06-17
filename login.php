5afedc513c9c3fd904e70a806adaf6014d30f895
inyoung <inyoung424@gmail.com>
2020-06-18 01:56:55 +0900

    로그인 최종 오류 수정

--- login.php
+++ login.php
@@ -1,43 +1,7 @@
-<<<<<<< HEAD
-<?
-  include './dbconn.php';
-
-  // user_id와 user_password를 받아옵니다.
-  $id = $_POST['user_id'];
-  $pwd = $_POST['user_password'];
-
-  // 같은 id가 있는지 중복을 체크합니다.
-  $query = "SELECT * FROM member_info WHERE member_id = '$id'";
-  $result = mysqli_query($conn, $query);
-  $num = mysqli_num_rows($result);
-
-  // 데이터 받아온 것을 넘깁니다.
-  // 중복된 것이 없으면 if(!0) 하여 if문의 조건을 만족하게 됩니다.
-  if(!$num)
-  {
-    $query2 = "INSERT INTO member_info (member_id, member_pw) VALUES ('$id', '$pwd')";
-    //$query3 = "INSERT INTO member_info(member_pw) VALUES ('$password')";
-    mysqli_query($conn, $query2);
-    //mysqli_query($conn, $query3);
-    echo "<script> alert('Welcome!'); </script>";
-    //location.href='./main.php'</script>";
-  }
-  else
-  {
-
-    // <script>
-    //   alert('Duplicated Id');
-    //   location.href = './index.html';
-    // </script>
-    echo "<script> alert('Duplicated Id'); </script>";
-    //location.href='./login_form.php'</script>";
-  }
-=======
 <?php
 session_start();
 if($_SESSION['user_id'] == null){ //로그인x
 ?>
->>>>>>> a406d0a2cfa1f0798404e86f9e0763f72dabdb2d
 
 <center><br><br><br>
 <form name="login_form" action="login_check.php" method="post">
--- logout.php
+++ logout.php
@@ -5,11 +5,7 @@ session_start();
  {
   session_destroy();
   echo '<h1> 로그아웃 하였습니다. </h1>';
-<<<<<<< HEAD
-  echo "<script>window.alert('logout 완료');</script>";
-=======
   echo "<script>window.alert('로그아웃 완료');</script>";
->>>>>>> a406d0a2cfa1f0798404e86f9e0763f72dabdb2d
 }
   echo '<h1> 로그인 상태가 아닙니다. </h1>';
 echo "<script>location.href='index.html';</script>";
