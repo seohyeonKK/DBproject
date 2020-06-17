845041e563d98632fe34e27ba37ac7cfe021cf86
seohyeon <sts05158@gmail.com>
2020-06-17 18:05:06 +0900

    DB와의 연동 성공

--- dbconn.php
+++ dbconn.php
@@ -1,19 +1,14 @@
 <?
-  /* db_project에 연결합니다. */
-  $host_name = "localhost";
-  $db_user_id = "root";
-  $db_pwd = "0000";
-  $db_name = "db_project"
-  $conn = mysqli_connect($host_name, $db_user_id, $db_pwd, $db_name);
+     $host_name = "localhost";
+     $db_user_id = "db_admin";
+     $db_pwd = "0000";
+     $db_name = "db_project";
+     $conn = mysqli_connect($host_name, $db_user_id, $db_pwd, $db_name);
 
-  /* 연결을 체크합니다. */
-  if($conn->connect_error)
-  {
-    printf("Connect failed: %s\n", $conn->connect_error);
-    exit();
-  }
-  
-
-  //안녕하세요
-
+     /* check connection */
+     if ($conn->connect_error)
+     {
+       printf("Connect failed: %s\n", $conn->connect_error);
+       exit();
+     }
 ?>
--- login.php
+++ login.php
@@ -6,7 +6,7 @@
   $pwd = $_POST['user_password'];
 
   // 같은 id가 있는지 중복을 체크합니다.
-  $query = "SELECT * FROM member_info WHERE id = '$member_id'";
+  $query = "SELECT * FROM member_info WHERE member_id = '$id'";
   $result = mysqli_query($conn, $query);
   $num = mysqli_num_rows($result);
 
@@ -14,18 +14,22 @@
   // 중복된 것이 없으면 if(!0) 하여 if문의 조건을 만족하게 됩니다.
   if(!$num)
   {
-    $query2 = "INSERT INTO member_info VALUES ('$id', '$pwd')";
+    $query2 = "INSERT INTO member_info (member_id, member_pw) VALUES ('$id', '$pwd')";
+    //$query3 = "INSERT INTO member_info(member_pw) VALUES ('$password')";
     mysqli_query($conn, $query2);
-    echo "<script> alert('Welcome!'); location.href='./index.html'</script>";
+    //mysqli_query($conn, $query3);
+    echo "<script> alert('Welcome!'); </script>";
+    //location.href='./main.php'</script>";
   }
   else
   {
-    
+
     // <script>
     //   alert('Duplicated Id');
     //   location.href = './index.html';
     // </script>
-    echo "<script> alert('Duplicated Id'); location.href='./index.html'</script>";
+    echo "<script> alert('Duplicated Id'); </script>";
+    //location.href='./login_form.php'</script>";
   }
 
 
