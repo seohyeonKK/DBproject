0bcae5b5f85d94008c93fa7c696e6204ac5b20b2
inyoung <inyoung424@gmail.com>
2020-06-18 13:38:30 +0900

    특정 계좌의 사기 내역 조회하는 페이지 생성

--- find.php
+++ find.php
@@ -13,19 +13,6 @@ include './dbconn.php';
 
   echo"<br><br>";
 
-  // $query = "SELECT * FROM cheater_info WHERE account='$account'";
-  // $result = mysqli_query($conn, $query);
-  // $row = mysqli_fetch_array($result);
-  // $cheater_code = $row["cheater_code"];
-  //
-  // if ($row) {
-  //   echo"
-  //     <tr>
-  //       <td><a href='content.php?id=$row[cheater_code]'>$row[cheater_code]</a></td>
-  //       <td>$row[account]</td>
-  //     </tr>";
-  // }
-  // mysqli_close($conn);
 
  ?>
 
@@ -57,38 +44,39 @@ include './dbconn.php';
        <table width=580 border=0 cellpadding=2 cellspacing=1 bgcolor="#777777" >
          <tr>
            <td height=20 align=center bgcolor="#999999">
-           <font color=white><b>사기 이력 조회</b></font></td></tr>
+             <font color=white><b>사기 이력 조회</b></font>
+           </td>
+         </tr>
          <tr>
-           <td bgcolor=white>&nbsp;
+           <td bgcolor=white >&nbsp;
             <table align=center>
-                <tr>
-                 <td width=160 aligh=left>조회할 계좌</td>
-                 <td align=left><input type=text name="account" size=25 maxlength=20 >
-                </tr>
+              <tr>
+                <td width=160 aligh=left>조회할 계좌</td>
+                <td align=left><input type=text name="account" size=25 maxlength=20 ></td>
+              </tr>
             </table>
             <table align=center>
               <tr>
                 <td>
-                  <br>
-                  <center>
-                   <input type="button" value="검색" name="search_submit" onclick="searchAccount()"> &nbsp;&nbsp;
-                   <input type="button" value="메인페이지" name="back" onclick="location.href='main.php'">
-                   </center>
-                   <br>
-                 </td>
-               </tr>
-             </table>
+                  <br><center>
+                    <input type="button" value="검색" name="search_submit" onclick="searchAccount()"> &nbsp;&nbsp;
+                    <input type="button" value="메인페이지" name="back" onclick="location.href='main.php'">
+                  </center><br>
+                </td>
+              </tr>
+            </table>
+          </td>
          </tr>
        </table>
      </form>
 
    </body>
- </html>
+   <br>
 
- <?
+
+<?
  //session_start();
  //include './dbconn.php';
-
  $account = $_POST['account'];
 
  $query = "SELECT * FROM cheater_info WHERE account='$account'";
@@ -96,16 +84,37 @@ include './dbconn.php';
  $row = mysqli_fetch_array($result);
  $cheater_code = $row["cheater_code"];
 
- if ($row) {
-   echo"
-     <tr>
-       <td><a href='content.php?id=$row[cheater_code]'>$row[cheater_code]</a></td>
-       <td>$row[account]</td>
-     </tr>";
+ $string = "계좌를 입력해주세요.";
+
+ if($account)
+ {
+   echo $account;
+   echo "를 조회한 결과입니다.<br><br>";
  }
- else{
-   echo"<td> <p> 등록된 계좌가 없습니다.</p></td>";
+
+ if ($row)
+ {
+    $string = "사기 이력이 있습니다.";
+    echo $string;
+    echo "<br><br>";
+    echo "<a href='find_list.php?account=$account'>사기 내역 조회를 원하시면 클릭하세요.</a>";
+ }
+ else
+ {
+   if(!$account)
+   {
+     $string = "계좌를 입력해주세요.";
+   }
+   else
+   {
+     $string = "등록된 계좌가 없습니다.";
+   }
+   //cho"<td> <p> 등록된 계좌가 없습니다.</p></td>";
+   echo $string;
  }
  mysqli_close($conn);
 
 ?>
+
+
+ </html>
